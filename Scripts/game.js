5bacd34e3ae24d8c33f1a01b58b17f37806519ef
Sergi <sgirona98@boscdelacoma.cat>
2023-05-04 09:15:22 +0200

    Update

--- Scripts/game.js
+++ Scripts/game.js
@@ -3,8 +3,8 @@ import Jugador from "./jugador.js";
 export default class Game{
     constructor(nom) {
         this.player= new Jugador(nom);
+        this.player2= new Jugador();
         this.npc1= new Jugador();
-        this.npc2= new Jugador();
         this.currentPlayer= this.player;
     }
 
--- Scripts/virusCard.js
+++ Scripts/virusCard.js
@@ -4,7 +4,4 @@ export default class Virus extends Card{
         super(color);
         this.tipus="virus";
     }
-    infectar(){
-
-    }
 }
